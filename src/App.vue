ab3edc9c188617a2fba62f71255a7a599eaa8c5e
yoherman2076 <german@avanzaprl.es>
2026-06-18 13:16:08 +0200

    fix: add WSL to the project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div v-if="pokemon">
+    <h1>{{ pokemon.name }}</h1>
+    
+  </div>
+  
 </template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const pokemon = ref(null)
+
+try {
+  const response = await axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+  pokemon.value = response.data
+}
+
+catch(error) {
+  console.error(error)
+}
+
+</script>
