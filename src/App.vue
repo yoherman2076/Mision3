bf5341a31b53df19db6a1506ad596a1f97afe752
yoherman2076 <german@avanzaprl.es>
2026-06-19 13:03:18 +0200

    feature: add general structure of the project, logic of the random button, display of the sprite with the poke id and form.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,74 @@
 <template>
-  <div v-if="pokemon">
-    <h1>{{ pokemon.name }}</h1>
-    
+  <h1>Welcome to la Misión 3</h1>
+  <br>
+  
+  <!-- formulario -->
+  <form @submit.prevent="sendDatos">
+    <input v-model="form.name" placeholder="Nombre" class="bg-blue-200 mx-30"/>
+    <input v-model="form.email" placeholder="Email" class="bg-blue-200 mx-30"/>
+    <button type="submit">Enviar</button>
+  </form>
+  <br>
+
+  <!-- cargación -->
+  <div v-if="loading">
+    Cargando . . .
+  </div>
+  <div v-else-if="pokemon">
+    <h2
+      class="text-center font-bold"
+    >{{ pokemon.name }}</h2>
   </div>
   
+  <!-- Botón randomizador -->
+  <RandomButton @random-click="getRandomPokemon" />
+
+  <img v-if="sevenUp" :src="sevenUp" alt="poKimon" class="w-80 h-80" >
+
 </template>
 
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import axios from 'axios'
+import RandomButton from "./components/RandomButton.vue"
 
-const pokemon = ref(null)
+const apiURL = import.meta.env.VITE_API_URL
+const loading = ref(false)
+const pokemon = ref<any>(null)
+const sevenUp = ref('')
 
-try {
-  const response = await axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
-  pokemon.value = response.data
+
+const form = ref({
+  name: "",
+  email: ""
+})
+
+const sendDatos = () => {
+  console.log("Datos enviados:", form)
+
+  form.value.name = ""
+  form.value.email = ""
 }
 
-catch(error) {
-  console.error(error)
+async function getRandomPokemon() {
+  try {
+    const randomId = Math.floor(Math.random() * 1025) + 1
+  
+    const response = await axios.get(`${apiURL}/pokemon/${randomId}`)
+
+    pokemon.value = response.data
+    sevenUp.value = pokemon.value.sprites.front_default
+
+  }
+  catch (error) {
+    console.error("Error garrafal al obtener el pookemon:", error)
+  }
+  finally {
+    await new Promise(resolve => setTimeout(resolve, 500))
+    loading.value = false
+  }
+
 }
 
 </script>
---
+++ src/components/RandomButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="$emit('random-click')"
+    class="bg-blue-600 text-white px-10 py-3 rounded"
+  >
+    Pokemon aleatorio
+  </button><br>
+
+  <img v-if="sevenUp" :src="sevenUp" alt="poKimon" >
+
+</template>
+
+<!-- División divisidora -->
+
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const sevenUp = ref('')
+defineEmits<{
+    (e: "random-click"): void
+}>()
+</script>
